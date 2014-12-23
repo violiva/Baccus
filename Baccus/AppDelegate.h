e5a1694c0ba367d64bb167b53c296e58371065f8
Vicente Oliva <oliva.vicente@gmail.com>
2014-12-23 09:50:24 +0100

    Initial Commit

---
+++ Baccus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baccus
+//
+//  Created by Vicente Oliva de la Serna on 23/12/14.
+//  Copyright (c) 2014 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baccus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Baccus
+//
+//  Created by Vicente Oliva de la Serna on 23/12/14.
+//  Copyright (c) 2014 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
